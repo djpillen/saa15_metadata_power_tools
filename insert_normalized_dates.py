3480dce2418fa835a634e1c5e4a54ff7d360c77c
Dallas <djpillen@umich.edu>
2015-08-18 10:56:36 -0400

    finalizing files

--- insert_normalized_dates.py
+++ insert_normalized_dates.py
@@ -3,14 +3,16 @@ from lxml import etree
 from os.path import join
 
 path = 'demo_eads_normalized'
+normalized_csv = 'normalized_dates.csv'
+normalized_count = 0
 
-with open('normalized_dates.csv', 'rb') as csvfile:
+with open(normalized_csv, 'rb') as csvfile:
     reader = csv.reader(csvfile)
     next(reader, None)
     for row in reader:
         filename = row[0]
         print filename
-        xpath = row[1]
+        xpath = row[4]
         normalized = row[3]
         ead_file = open(join(path, filename))
         tree = etree.parse(ead_file)
@@ -19,5 +21,7 @@ with open('normalized_dates.csv', 'rb') as csvfile:
         outfile = open(join(path, filename), 'w')
         outfile.write(etree.tostring(tree, encoding="utf-8", xml_declaration=True))
         outfile.close()
+        normalized_count += 1
 
-print "Normalization Complete"
+print "Normalization based on contents of " + normalized_csv + " complete"
+print str(normalized_count) + " dates normalized"
--- non-normalized_unitdate_count.py
+++ non-normalized_unitdate_count.py
@@ -1,10 +1,13 @@
-import csv
 from lxml import etree
 import os
 from os.path import join
 import re
 
 path = 'demo_eads_normalized'
+
+total_dates = 0
+normalized_dates = 0 
+non_normalized_dates = 0
 
 undated = re.compile(r'^[Uu]ndated$')
 
@@ -13,10 +16,13 @@ for filename in os.listdir(path):
 	unitdates = tree.xpath('//unitdate')
 	for unitdate in unitdates:
 		if unitdate.text is not None:
+			total_dates += 1
 			if not 'normal' in unitdate.attrib and not undated.match(unitdate.text):
-				date = unitdate.text
-				date_path = tree.getpath(unitdate)
-				with open('non-normalized_unitdates.csv','ab') as csvfile:
-					writer = csv.writer(csvfile)
-					writer.writerow([filename, date_path, date])
-	print filename
+				non_normalized_dates += 1
+			else:
+				normalized_dates += 1
+
+
+print "Total dates:", total_dates
+print "Normalized dates:", normalized_dates
+print "Non-normalized dates:", non_normalized_dates
